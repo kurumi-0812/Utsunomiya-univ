8a578fc36f5df3c262bf6b6c4f831935677a6224
kurumi-0812 <kurumyu0812@kurumyu.com>
2021-03-01 15:15:59 +0900

    バーガーメニュー追加

--- Utsunomiya-univ/assets/js/main.js
+++ Utsunomiya-univ/assets/js/main.js
@@ -1,5 +1,14 @@
+
+
 //共通パーツ読み込み
-      $(function () {
-        $("#header").load("header.html");
-        $("#footer").load("footer.html");
-      });
+$(function () {
+  $("#header").load("header.html");
+  $("#footer").load("footer.html");
+});
+
+// バーガーメニュー
+$(function () {
+  $('.menu-btn').on('click', function () {
+    $('.menu').toggleClass('is-active');
+  });
+}());
