044264211708a5682f1b08adc505481d972cb5a0
kurumi-0812 <kurumyu0812@kurumyu.com>
2021-03-04 16:21:39 +0900

    バーガーメニュー修正

--- Utsunomiya-univ/assets/js/main.js
+++ Utsunomiya-univ/assets/js/main.js
@@ -8,8 +8,21 @@ $(function () {
 });
 
 // バーガーメニュー
-$(function () {
-  $(document).on('click','.burger-menu_btn', function (){
-    $('.burger-menu').toggleClass('is-active');
-  });
-}());
+$(function() {
+  $(document).on('click','.hamburger', function (){
+    // $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+});
+
+// $(function () {
+//   $(document).on('click','.burger-menu_btn', function (){
+//     $('.burger-menu').toggleClass('is-active');
+//   });
+// }());
